edc1611a4225bf0ea292de02b915d71907e5a91e
ajuroshan <ajuaju0483@gmail.com>
2024-08-28 15:14:28 +0530

    scan qr fronend in progress

--- mess/urls.py
+++ mess/urls.py
@@ -4,8 +4,9 @@ from .views import *
 
 urlpatterns = [
     path('apply_messcut/',apply_for_messcut,name='apply_for_messcut'),
-    path('mark_attendance/',mark_attendance,name='mark_attendance'),
     path('dashboard/',dashboard,name='dashboard'),
     path('mess_bill/',view_mess_bill,name='view_mess_bill'),
     path('weekly_menu/',weekly_menu,name='weekly_menu'),
+    path('scan_qr/',scan_qr,name='scan_qr'),
+    path('mark_attendance/',mark_attendance,name='mark_attendance'),
     ]
--- mess/views.py
+++ mess/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
 from application.models import Application
@@ -7,8 +7,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 
-
 import datetime
+
+
 # Create your views here.
 @login_required
 def apply_for_messcut(request):
@@ -34,7 +35,12 @@ def apply_for_messcut(request):
 	else:
 		form = MesscutForm()
 
-	return render(request, 'mess/apply.html', {'form': form, 'total_messcut_days': total_messcut_days, 'messcuts': messcuts})
+	return render(request, 'mess/apply.html',
+	              {'form': form, 'total_messcut_days': total_messcut_days, 'messcuts': messcuts})
+
+
+def scan_qr(request):
+	return render(request, 'mess/scan_qr.html')
 
 
 @login_required
@@ -44,6 +50,7 @@ def mark_attendance(request):
 		print(request.POST)
 		qr_code_data = request.POST.get('qr_code_data')
 		mess_no = request.POST.get('mess_no')
+		confirm = request.POST.get('confirm') == 'true'
 
 		if mess_no and not qr_code_data:
 			qr_code_data = mess_no
@@ -51,31 +58,66 @@ def mark_attendance(request):
 		try:
 			qr_code_data = int(qr_code_data)
 		except ValueError:
-			return JsonResponse({"status": "error", "message": "Invalid QR Code"}, status=400)
+			return JsonResponse({
+				"status"     : "error",
+				"message"    : "Invalid QR Code",
+				"app_details": {}
+			}, status=400)
 
 		# Find the application using the mess_no from the QR code
 		application = Application.objects.filter(mess_no=qr_code_data).first()
 		if application:
+			app_details = {
+				"full_name"   : f"{application.applicant.first_name + ' ' + application.applicant.last_name }",  # Adjust according to your model fields
+				"dept": str(application.department).upper(),
+				"mess_no": application.mess_no,
+			}
 			messcuts = application.messcuts.all()
 			if messcuts:
 				for messcut in messcuts:
 					if messcut.start_date <= date.today() <= messcut.end_date:
-						return JsonResponse(
-							{"status": "error", "message": "You cannot mark attendance during a messcut period"},
-							status=400)
-			else:
+						return JsonResponse({
+							"status"     : "error",
+							"message"    : "You cannot Mark Attendance during messcut period. ",
+							"app_details": app_details,
+							"messcut": f"\nMesscut from {messcut.start_date} to  {messcut.end_date} "
+						}, status=400)
+			elif confirm:
 				# Create the attendance record, linking it to the current user
 				try:
 					attendance = MessAttendance.objects.create(student=application)
 					attendance.save()
+					return JsonResponse({
+						"status"     : "success",
+						"message"    : "Attendance marked successfully",
+						"app_details": app_details
+					}, status=200)
 				except Exception as e:
-					return JsonResponse({"status": "error", "message": f"Attendance already marked: {e}"}, status=400)
-
-				return JsonResponse({"status": "success", "message": "Attendance marked successfully"})
+					return JsonResponse({
+						"status"     : "error",
+						"message"    : f"Attendance already marked: {e}",
+						"app_details": app_details
+					}, status=400)
+			else:
+				return JsonResponse({
+					"status"     : "error",
+					"message"    : f"Mark Attendance",
+					"app_details": app_details
+				}, status=400)
 		else:
-			return JsonResponse({"status": "error", "message": "Application not found"}, status=404)
+			return JsonResponse({
+				"status"     : "error",
+				"message"    : f"No Application found",
+			}, status=400)
+	else:
+		return JsonResponse({
+			"status"     : "error",
+			"message"    : "Get req",
+			"app_details": {}
+		}, status=404)
 
-	return render(request, 'mess/scan_qr.html')
+
+
 
 @login_required
 def dashboard(request):
@@ -93,12 +135,11 @@ def calculate_mess_bill():
 	FEAST_CHARGES = 0
 
 	for application in Application.objects.filter(accepted=True):
-
 		# Calculate the total amount
 		messcuts = application.messcuts.filter(start_date__month=date.today().month)
 		total_messcut_days = sum((messcut.end_date - messcut.start_date).days + 1 for messcut in messcuts)
 		effective_days = TOTAL_DAYS - total_messcut_days
-		total_amount = (AMOUNT_PER_DAY * effective_days) + (ESTABLISHMENT_CHARGES + OTHER_CHARGES+FEAST_CHARGES)
+		total_amount = (AMOUNT_PER_DAY * effective_days) + (ESTABLISHMENT_CHARGES + OTHER_CHARGES + FEAST_CHARGES)
 		mess_bill = application.mess_bill.create(
 			amount=total_amount,
 			month=MONTH,
@@ -117,18 +158,19 @@ def calculate_mess_bill():
 		# Save the application
 		application.save()
 
+
 @login_required
 def view_mess_bill(request):
 	application = Application.objects.filter(applicant=request.user).first()
 	mess_bill = application.mess_bill.filter(month__month=date.today().month).last()
-	return render(request, 'mess/view_mess_bill.html', {'mess_bill': mess_bill,"application":application})
+	return render(request, 'mess/view_mess_bill.html', {'mess_bill': mess_bill, "application": application})
 
 
 @login_required
 def weekly_menu(request):
-    weekly_menu = Messmenu.objects.all().order_by('id')  # Get the full week's menu
+	weekly_menu = Messmenu.objects.all().order_by('id')  # Get the full week's menu
 
-    context = {
-        'weekly_menu': weekly_menu,
-    }
-    return render(request, 'mess/menu.html', context)
+	context = {
+		'weekly_menu': weekly_menu,
+	}
+	return render(request, 'mess/menu.html', context)
