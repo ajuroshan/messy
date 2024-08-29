bde960095f988dae5af12a24f2977543a6257de0
ajuroshan <ajuaju0483@gmail.com>
2024-08-30 01:43:42 +0530

    cleaned some code

--- application/admin.py
+++ application/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import Group
 
 # Register your models here.
 from .models import *
@@ -9,7 +11,8 @@ class ApplicationAdmin(admin.ModelAdmin):
 	list_display = ['get_applicant_name', 'department', 'semester', 'mess_no', 'accepted', 'created_at']
 	list_filter = ['applicant__first_name', 'applicant__last_name', 'hostel', 'mess_no', 'accepted', 'created_at']
 	search_fields = ['applicant__first_name', 'applicant__last_name', 'hostel', 'accepted', 'created_at']
-	actions = ['accept_application', 'cancel_application']
+	actions = ['accept_application', 'cancel_application', 'make_official_outmess', 'make_mess_assistant',
+	           'dismiss_mess_assistant']
 
 	def get_applicant_name(self, obj):
 		return f"{obj.applicant.first_name} {obj.applicant.last_name}"
@@ -34,13 +37,62 @@ class ApplicationAdmin(admin.ModelAdmin):
 
 	cancel_application.short_description = "Cancel selected Applications"
 
+	def make_mess_assistant(self, request, queryset):
+		"""
+		Mark selected applications as mess_assistants.
+		"""
+		# Get or create the mess_assistants group
+		mess_assistants_group, created = Group.objects.get_or_create(name='mess_assistants')
+
+		# Iterate over the selected applications
+		for application in queryset:
+			# Fetch the applicant user from the application
+			user = application.applicant
+
+			# Add the user to the mess_assistants group
+			if not user.groups.filter(name='mess_assistants').exists():
+				user.groups.add(mess_assistants_group)
+				self.message_user(request, f'User {user.username} added to mess_assistants group.')
+			else:
+				self.message_user(request, f'User {user.username} is already a mess_assistant.')
+
+		# Optionally, you might want to refresh the changelist view
+		return HttpResponseRedirect(request.get_full_path())
+
+	make_mess_assistant.short_description = "Make selected applications mess assistants"
+
+	def dismiss_mess_assistant(self, request, queryset):
+		"""
+		Dismiss selected applications as mess_assistants.
+		"""
+		# Get or create the mess_assistants group
+		mess_assistants_group, created = Group.objects.get_or_create(name='mess_assistants')
+
+		# Iterate over the selected applications
+		for application in queryset:
+			# Fetch the applicant user from the application
+			user = application.applicant
+
+			# Add the user to the mess_assistants group
+			if user.groups.filter(name='mess_assistants').exists():
+				user.groups.remove(mess_assistants_group)
+				self.message_user(request, f'User {user.username} removed from mess_assistants group.')
+			else:
+				self.message_user(request, f'User {user.username} is not a mess_assistant.')
+
+		# Optionally, you might want to refresh the changelist view
+		return HttpResponseRedirect(request.get_full_path())
+
+	dismiss_mess_assistant.short_description = "Dismiss selected mess assistants"
+
 
 @admin.register(AcceptedApplication)
 class AcceptedApplicationAdmin(admin.ModelAdmin):
 	list_display = ['get_applicant_name', 'department', 'semester', 'mess_no', 'accepted', 'created_at']
 	list_filter = ['applicant', 'hostel', 'mess_no', 'accepted', 'created_at']
 	search_fields = ['applicant', 'hostel', 'accepted', 'created_at']
-	actions = ['accept_application', 'cancel_application', 'make_official_outmess']
+	actions = ['accept_application', 'cancel_application', 'make_official_outmess', 'make_mess_assistant',
+	           'dismiss_mess_assistant']
 
 	def get_applicant_name(self, obj):
 		return f"{obj.applicant.first_name} {obj.applicant.last_name}"
@@ -73,6 +125,55 @@ class AcceptedApplicationAdmin(admin.ModelAdmin):
 		self.message_user(request, f"{updated_count} applications were successfully marked as verified.")
 
 	make_official_outmess.short_description = "Mark students as official outmess"
+
+	# add to mess_assistants group
+	def make_mess_assistant(self, request, queryset):
+		"""
+		Mark selected applications as mess_assistants.
+		"""
+		# Get or create the mess_assistants group
+		mess_assistants_group, created = Group.objects.get_or_create(name='mess_assistants')
+
+		# Iterate over the selected applications
+		for application in queryset:
+			# Fetch the applicant user from the application
+			user = application.applicant
+
+			# Add the user to the mess_assistants group
+			if not user.groups.filter(name='mess_assistants').exists():
+				user.groups.add(mess_assistants_group)
+				self.message_user(request, f'User {user.username} added to mess_assistants group.')
+			else:
+				self.message_user(request, f'User {user.username} is already a mess_assistant.')
+
+		# Optionally, you might want to refresh the changelist view
+		return HttpResponseRedirect(request.get_full_path())
+
+	make_mess_assistant.short_description = "Make selected applications mess assistants"
+
+	def dismiss_mess_assistant(self, request, queryset):
+		"""
+		Dismiss selected applications as mess_assistants.
+		"""
+		# Get or create the mess_assistants group
+		mess_assistants_group, created = Group.objects.get_or_create(name='mess_assistants')
+
+		# Iterate over the selected applications
+		for application in queryset:
+			# Fetch the applicant user from the application
+			user = application.applicant
+
+			# Add the user to the mess_assistants group
+			if user.groups.filter(name='mess_assistants').exists():
+				user.groups.remove(mess_assistants_group)
+				self.message_user(request, f'User {user.username} removed from mess_assistants group.')
+			else:
+				self.message_user(request, f'User {user.username} is not a mess_assistant.')
+
+		# Optionally, you might want to refresh the changelist view
+		return HttpResponseRedirect(request.get_full_path())
+
+	dismiss_mess_assistant.short_description = "Dismiss selected mess assistants"
 
 	def get_queryset(self, request):
 		return Application.objects.filter(accepted=True).order_by("-created_at")
