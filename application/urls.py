02f85794ec058815225bc0ee0518eb1906f2a138
ajuroshan <ajuaju0483@gmail.com>
2026-01-12 21:35:22 +0530

    fix

--- application/urls.py
+++ application/urls.py
@@ -4,6 +4,6 @@ from .views import *
 
 urlpatterns = [
     path("", home, name="home"),
-    # path("apply/", apply, name="apply"),
-    # path("login/", login, name="login"),
+    path("apply/", apply, name="apply"),
+    path("login/", login, name="login"),
 ]
