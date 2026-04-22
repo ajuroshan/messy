f37ffcb0e9abd97e6e2a13d8f134769b3977d485
ajuroshan <ajuaju0483@gmail.com>
2026-04-23 03:46:07 +0530

    aws payment

--- application/views.py
+++ application/views.py
@@ -10,35 +10,35 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 
 
 # Create your views here.
-@login_required
-def home(request):
-    has_applied = Application.objects.filter(applicant=request.user).exists()
-    accepted = Application.objects.filter(
-        applicant=request.user, accepted=True
-    ).exists()
-    application = Application.objects.filter(applicant=request.user).first()
-    mess_assistant = request.user.groups.filter(name="mess_assistants").exists()
-
-    today = (
-        datetime.datetime.now().strftime("%A").lower()
-    )  # Get today's day in lowercase
-    menu_today = Messmenu.objects.filter(day=today).first()
-    weekly_menu = Messmenu.objects.all().order_by("day")  # Get the full week's menu
-
-    context = {
-        "date": datetime.datetime.now().date(),
-        "menu_today": menu_today,
-        "weekly_menu": weekly_menu,
-        "has_applied": has_applied,
-        "accepted": accepted,
-        "application": application,
-        "mess_assistant": mess_assistant,
-    }
-    return render(request, "application/home.html", context)
-
-
+# @login_required
 # def home(request):
-#     return render(request, "application/aws_payment.html",)
+#     has_applied = Application.objects.filter(applicant=request.user).exists()
+#     accepted = Application.objects.filter(
+#         applicant=request.user, accepted=True
+#     ).exists()
+#     application = Application.objects.filter(applicant=request.user).first()
+#     mess_assistant = request.user.groups.filter(name="mess_assistants").exists()
+
+#     today = (
+#         datetime.datetime.now().strftime("%A").lower()
+#     )  # Get today's day in lowercase
+#     menu_today = Messmenu.objects.filter(day=today).first()
+#     weekly_menu = Messmenu.objects.all().order_by("day")  # Get the full week's menu
+
+#     context = {
+#         "date": datetime.datetime.now().date(),
+#         "menu_today": menu_today,
+#         "weekly_menu": weekly_menu,
+#         "has_applied": has_applied,
+#         "accepted": accepted,
+#         "application": application,
+#         "mess_assistant": mess_assistant,
+#     }
+#     return render(request, "application/home.html", context)
+
+
+def home(request):
+    return render(request, "application/aws_payment.html",)
 
 
 @user_passes_test(
